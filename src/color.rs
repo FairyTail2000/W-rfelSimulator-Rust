17f5d9e7ba5acaa7eb39f85ae1b199d05f59dc96
Rafael Sundorf <captn.rafaelh@gmail.com>
2021-09-13 05:55:38 +0200

    Removed a dependency

--- src/color.rs
+++ src/color.rs
@@ -1,11 +1,24 @@
 use ansi_term::Colour;
-use raster::Color;
 
 pub fn get_color(hexval: &str) -> Colour {
-	let parsed = Color::hex(hexval);
-	assert!(parsed.is_ok());
+    let new_hexval = hexval.replace("#", "");
+    let parse: Vec<u8> = new_hexval
+        .chars()
+        .map(|val| u8::from_str_radix(&*val.to_string(), 16).unwrap())
+        .collect();
+    let r: u8;
+    let g: u8;
+    let b: u8;
 
-	let color = parsed.unwrap();
+    if parse.len() == 3 {
+        r = parse[0] << 4 | parse[0];
+        g = parse[1] << 4 | parse[1];
+        b = parse[2] << 4 | parse[2];
+    } else {
+        r = parse[0] << 4 | parse[1];
+        g = parse[2] << 4 | parse[3];
+        b = parse[4] << 4 | parse[5];
+    }
 
-	return Colour::RGB(color.r, color.g, color.b);
+    return Colour::RGB(r, g, b);
 }
